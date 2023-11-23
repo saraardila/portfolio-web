3ede8b879e241d5c640995b6b679d8504e7eb215
saraardila <saraardiladam@gmail.com>
2023-11-23 19:55:31 +0100

    Implementacion de correos

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,43 +1,74 @@
-import React from 'react';
-import "./contact.css"
-
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import "./contact.css";
 
 const Contact = () => {
+  const [formStatus, setFormStatus] = useState(null);
+
+  const enviarCorreo = async (e) => {
+    e.preventDefault();
+
+    // Validar campos obligatorios
+    const name = e.target.from_name.value.trim();
+    const email = e.target.from_email.value.trim();
+    const message = e.target.message.value.trim();
+
+    if (!name || !email || !message) {
+      setFormStatus('emptyFields');
+      return;
+    }
+
+    try {
+      await emailjs.sendForm('service_ab7rs1t', 'template_015nfln', e.target, 'xWkr_2_KKbCzMPkAO');
+      console.log('Correo enviado correctamente');
+      setFormStatus('success');
+    } catch (error) {
+      console.error('Error al enviar el correo:', error);
+      setFormStatus('error');
+    }
+
+    // Reset the form after submission
+    e.target.reset();
+  };
+
   return (
-   
-     <section className="contact container section" id='contact'>
+    <section className="contact container section" id='contact'>
       <h2 className="section__title">
-      Get In Touch
+        Ponte en contacto
       </h2>
 
       <div className="contact__container grid">
         <div className="contact__info">
-          <h3 className="contact__title">Let's talk about everything!</h3>
-          <p className="contact__details">Don't like forms? Send me an email.👋</p>
+          <h3 className="contact__title">¡Hablemos!</h3>
+          <p className="contact__details">¿No te gustan los formularios? Envíame un correo electrónico: saraardila.contacto@gmail.com👋</p>
         </div>
 
-        <form action="" className="contact__form">
+        <form className="contact__form" onSubmit={enviarCorreo}>
           <div className="contact__form-group">
             <div className="contact__form-div">
-              <input type="text" className="contact__form-input" placeholder='Insert your name'/>
+              <input type="text" className="contact__form-input" name="from_name" placeholder='Ingresa tu nombre' />
             </div>
             <div className="contact__form-div">
-              <input type="email" className="contact__form-input" placeholder='Insert your email'/>
+              <input type="email" className="contact__form-input" name="from_email" placeholder='Ingresa tu correo electrónico' />
             </div>
           </div>
 
           <div className="contact__form-div">
-              <input type="text" className="contact__form-input" placeholder='Insert your subject'/>
-            </div>
-            <div className="contact__form-div contact__form-area">
-              <textarea name="" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write yout message'></textarea>
-            </div>
+            <input type="text" className="contact__form-input" name="input_subject" placeholder='Ingresa tu asunto' />
+          </div>
+          <div className="contact__form-div contact__form-area">
+            <textarea name="message" cols="30" rows="10" className='contact__form-input' placeholder='Escribe tu mensaje'></textarea>
+          </div>
 
-            <button className="btn">Send Message</button>
+          {formStatus === 'emptyFields' && <p style={{ color: 'red' }}>Por favor, completa todos los campos obligatorios</p>}
+          {formStatus === 'success' && <p style={{ color: 'green' }}>Correo enviado correctamente</p>}
+          {formStatus === 'error' && <p style={{ color: 'red' }}>Error al enviar el correo</p>}
+
+          <button type="submit" className="btn">Enviar Mensaje</button>
         </form>
       </div>
-     </section>
+    </section>
   )
 }
 
-export default Contact
+export default Contact;
